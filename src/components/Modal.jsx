97ac1b08877921d298674cc4f2236a23f8fae1f9
mybess00 <betancourtrafael00@gmail.com>
2023-03-26 23:14:24 -0400

    adding payment methods

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -15,13 +15,12 @@ export default function Modal({ id, name, qrContent, isVisible, handleVisibility
     toast.success(`Se ha copiado la informacion de pago de ${name}`)
   }
   return (
-    <ReactModal isOpen={isVisible} overlayClassName='ReactModal__Overlay' role='dialog' style={{
+    <ReactModal isOpen={isVisible} overlayClassName='ReactModal__Overlay' role='dialog' className='modal' style={{
       content: {
         position: 'fixed',
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        background: '#fff',
         overflow: 'hidden',
         WebkitOverflowScrolling: 'touch',
         borderRadius: '4px',
@@ -35,14 +34,22 @@ export default function Modal({ id, name, qrContent, isVisible, handleVisibility
       }
     }}>
       <div>
-        <div className="flex flex-col items-center">
-            <button onClick={closeModal} className='place-self-end bg-slate-900 hover:bg-black duration-200 text-white rounded-full py-1 px-2 mr-[-15px]'>✕</button>
+        <div className="flex flex-col items-center dark:bg-slate-800">
+            <button onClick={closeModal} className='place-self-end bg-slate-900 hover:bg-black duration-200 text-white dark:text-black dark:bg-slate-300 dark:hover:bg-slate-500 rounded-full py-1 px-2 mr-[-15px]'>✕</button>
             <div className='flex flex-col items-center p-4'>
-              <h3 className="text-lg font-bold text-center">{name}</h3>
+              <h3 className="text-lg font-bold text-center dark:text-white">{name}</h3>
               <div className="flex flex-col items-center my-6">
-                <QRCodeSVG value={qrContent} size='200'/>
+                <QRCodeSVG value={qrContent} size='200'
+                  imageSettings={{
+                    src: `/images/icons/${id}.svg`,
+                    x: undefined,
+                    y: undefined,
+                    height: 36,
+                    width: 36,
+                    excavate: true,
+                  }}/>
               </div>
-              <div onClick={copyQrContent} className='flex flex-row flex-nowrap items-center gap-1 my-2 text-white cursor-pointer bg-slate-900 hover:bg-black rounded-xl px-4 py-1 duration-300'>
+              <div onClick={copyQrContent} className='flex flex-row flex-nowrap items-center gap-1 my-2 text-white cursor-pointer bg-slate-900 hover:bg-black dark:text-black dark:bg-slate-300 dark:hover:bg-slate-500 rounded-xl px-4 py-1 duration-300'>
                 <IoCopy/> 
                 <div>Copy</div>
               </div>    
